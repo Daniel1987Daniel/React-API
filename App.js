02de0db5e3e06d00ac4ec590a9218941904f97a5
Daniel Sládeček <83771369+Daniel1987Daniel@users.noreply.github.com>
2023-01-14 23:52:40 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react"
+
+const App = () => {
+
+  const url = "http://api.open-notify.org/iss-now.json"
+  let [latitude, setLatitude] = useState("")  
+  let [longitude, setLongitude] = useState("")
+  const [urlMap, setUrlMap] = useState("")
+  
+  const getCoordinates = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    latitude = data["iss_position"]["latitude"]
+    longitude = data["iss_position"]["longitude"]
+    setLatitude(latitude)
+    setLongitude(longitude)
+
+    const iss_lat = data["iss_position"]["latitude"]
+    const iss_long = data["iss_position"]["longitude"]
+    setUrlMap(
+     `https://en.mapy.cz/zakladni?x=${iss_long}&y=${iss_lat}z=10`
+    )}
+
+    useEffect(() => {
+      getCoordinates()
+    }, [])
+
+  return (
+    <div>   
+      <h1 className="title">API</h1>
+      <main>
+        <h4>Latitude</h4>
+        <p>{latitude}</p>
+        <h4>Longitude</h4>
+        <p>{longitude}</p>
+      </main>
+      <footer>
+        <a href={urlMap} rel="noreferrer" target="_blank">Open in maps</a>
+      </footer>
+    </div>
+  )
+}
+
+export default App
